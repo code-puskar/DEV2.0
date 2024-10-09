136c57abf45ed8fce88feacca43d30da7403e777
Puskar Debnath <puskardebnath14@gmail.com>
2024-10-09 13:03:39 +0530

    commit

---
+++ P1.js
@@ -0,0 +1,16 @@
+
+
+// console.log("hello world")
+// document.body.firstElementChild
+// document.body.firstElementChild.childNodes
+// document.body.firstElementChild.children
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+// //for targeting specific element use ID
+// document.querySelector(".box").style.backgroundColor ="green"
+
+/* FACTORIAL
+const factorial = n=>[...Array(n)].reduce((acc,_,i)=>acc * (i+1),1);
+ console.log(factorial(5));*/
+
+ 
